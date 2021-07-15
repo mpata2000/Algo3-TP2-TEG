d9bbf8de32a67ccb310512ca211d5da9f9ba850f
Andreskub <andreskubler@gmail.com>
2021-07-14 23:31:45 -0300

    Agrego cambios a mejorar

--- src/main/java/edu/fiuba/algo3/lectorJson/LectorDeJson.java
+++ src/main/java/edu/fiuba/algo3/lectorJson/LectorDeJson.java
@@ -1,23 +1,16 @@
 package edu.fiuba.algo3.lectorJson;
 
-import java.io.IOException;
-import java.io.Reader;
-import java.lang.reflect.Type;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
-
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import edu.fiuba.algo3.modelo.Pais;
 
-/*
-public class LectorDeJson {
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 
-    public static void main(String[] args){
+public class LectorDeJson {
+    /*
+    public class LectorDeJson {
         try {
             Reader myReader = Files.newBufferedReader(Paths.get("paises/Teg-Fronteras.json"));
             Gson gson = new Gson();
@@ -26,22 +19,19 @@ public class LectorDeJson {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 }
 */
-public class LectorDeJson {
-    public static void main(String[] args) {
-        String json = "[{Pais:'Francia'},{Pais:'Gran Bretaña'}]";
+    public static void lector(String[] args) {
+        String json = "[{'Pais': 'Francia'},{'Pais': 'Gran Bretaña'}]";
 
-        //Gson gson = new Gson();
         // create the type for the collection. In this case define that the collection is of type Dataset
-        Type datasetListType = new TypeToken<ArrayList<Pais>>(){}.getType();
-        List<Pais> datasets = new Gson().fromJson(json, datasetListType);
+        Type paisListType = new TypeToken<ArrayList<Pais>>(){}.getType();
+        List<Pais> datasets = new Gson().fromJson(json, paisListType);
         for (Pais dataset : datasets) {
             System.out.println(dataset.getNombre());
             //System.out.println(dataset.getAlbum_id());
         }
-
     }
 }
+
--- src/main/java/edu/fiuba/algo3/modelo/Pais.java
+++ src/main/java/edu/fiuba/algo3/modelo/Pais.java
@@ -88,5 +88,4 @@ public class Pais {
         return this.ejercito.tirarDados(cantidadDeDados);
     }
 
-
 }
