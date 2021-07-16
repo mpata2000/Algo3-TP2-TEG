ebc094e76d0d658188d513c7a3a8ade61b7222c6
Andreskub <andreskubler@gmail.com>
2021-07-15 23:57:16 -0300

    Corrijo lectura de JSON - Ahora obtiene una List<Object>

--- src/main/java/edu/fiuba/algo3/lectorJson/LectorDeJson.java
+++ src/main/java/edu/fiuba/algo3/lectorJson/LectorDeJson.java
@@ -2,37 +2,30 @@ package edu.fiuba.algo3.lectorJson;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-import edu.fiuba.algo3.modelo.Pais;
-
 import java.io.IOException;
 import java.io.Reader;
 import java.lang.reflect.Type;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Collection;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 public class LectorDeJson {
     public static void lector() {
-        //Aca simulamos con un Json
-        String json = "{'nombre': 'Francia','continente': 'Europa'}";
-        Map<?,?> userMap = new Gson().fromJson(json, Map.class);
-        System.out.println(userMap.get("nombre"));
-        //Reader jsonLeido = null;
-        /*try {
+        try {
+            //Lectura del archivo Json
             Reader jsonLeido = Files.newBufferedReader(Paths.get("paises/Teg-Fronteras.json"));
-            //Type datasetListType = new TypeToken<Collection<Pais>>() {}.getType();
-            Map<?,?> userMap = new Gson().fromJson(jsonLeido, Map.class);
-            //List<Pais> datasets = new Gson().fromJson(jsonLeido, datasetListType);
+            Type datasetListType = new TypeToken<ArrayList<Object>>() {}.getType();
+            List<Object> listaObjetosPais = new Gson().fromJson(jsonLeido, datasetListType);
+
+            //Imprimir Lista de Object
+            for (int i = 0; i < listaObjetosPais.size(); i++) {
+                System.out.println(listaObjetosPais.get(i));
+            }
         } catch (IOException e) {
             e.printStackTrace();
-        }*/
+        }
 
-
-        /*for (Pais dataset : datasets) {
-            System.out.println(dataset.getNombre());
-        }*/
     }
 }
 
