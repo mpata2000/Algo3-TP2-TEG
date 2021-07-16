d09915ab25e201e60f6e253bc3f05f8b59b00b2d
Andreskub <andreskubler@gmail.com>
2021-07-15 21:26:43 -0300

    Agrego llamado a lector de json

--- src/main/java/edu/fiuba/algo3/Main.java
+++ src/main/java/edu/fiuba/algo3/Main.java
@@ -1,7 +1,10 @@
 package edu.fiuba.algo3;
 
+import edu.fiuba.algo3.lectorJson.LectorDeJson;
+
 public class Main {
     public static void main(String[] args) {
         App.main(args);
+        LectorDeJson.lector();
     }
 }
