96676799b948125719c52b3606e51cdb7ad0f428
Andreskub <andreskubler@gmail.com>
2021-07-17 00:19:15 -0300

    Agrego y dejo funcionando la lectura de los archivos json

--- src/main/java/edu/fiuba/algo3/lectorJson/LectorDeJson.java
+++ src/main/java/edu/fiuba/algo3/lectorJson/LectorDeJson.java
@@ -1,31 +1,67 @@
 package edu.fiuba.algo3.lectorJson;
 
-import com.google.gson.Gson;
+import com.google.gson.*;
 import com.google.gson.reflect.TypeToken;
-import java.io.IOException;
-import java.io.Reader;
+
+import java.io.*;
 import java.lang.reflect.Type;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public class LectorDeJson {
-    public static void lector() {
+    public static void lectorPaises() {
         try {
             //Lectura del archivo Json
             Reader jsonLeido = Files.newBufferedReader(Paths.get("paises/Teg-Fronteras.json"));
-            Type datasetListType = new TypeToken<List<Object>>() {}.getType();
-            List<Object> listaObjetosPais = new Gson().fromJson(jsonLeido, datasetListType);
+            Type datasetListType = new TypeToken<Collection<ObjetoFronteras>>() {}.getType();
+            List<ObjetoFronteras> listaObjetosPais = new Gson().fromJson(jsonLeido, datasetListType);
 
+            /*
             //Imprimir Lista de Object
             for (int i = 0; i < listaObjetosPais.size(); i++) {
-                System.out.println(listaObjetosPais.get(i));
+                System.out.println((listaObjetosPais.get(i)).getPais());
+                System.out.println((listaObjetosPais.get(i)).getContinente());
+                System.out.println((listaObjetosPais.get(i)).getPaisesLimitrofes());
             }
+            */
+
+            //DEVOLVER listaObjetosPais
+
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
+    public static void lectorCartasPais(){
+        try {
+            //Lectura del archivo Json
+            Reader jsonLeido = Files.newBufferedReader(Paths.get("paises/Teg-Cartas.json"));
+            Type datasetListType = new TypeToken<Collection<ObjetoCartas>>() {}.getType();
+            List<ObjetoCartas> listaObjetosCartas = new Gson().fromJson(jsonLeido, datasetListType);
+
+            /*
+            //Imprimir Lista de Object
+            for (int i = 0; i < listaObjetosCartas.size(); i++) {
+                System.out.println((listaObjetosCartas.get(i)).getPais());
+                System.out.println((listaObjetosCartas.get(i)).getSimbolo());
+            }
+            */
+
+            //DEVOLVER listaObjetosCartas
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
+
+
+
+
+
+
+
+
 
