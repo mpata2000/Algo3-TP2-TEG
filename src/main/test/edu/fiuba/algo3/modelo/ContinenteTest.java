f66003926f51c066d037d84a3d6aeb5d3ed343e2
Martin Pata Fraile de Manerola <64995978+mpata2000@users.noreply.github.com>
2021-07-19 03:43:47 -0300

    Test Continentes

---
+++ src/main/test/edu/fiuba/algo3/modelo/ContinenteTest.java
@@ -0,0 +1,42 @@
+package edu.fiuba.algo3.modelo;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ContinenteTest {
+    ArrayList<Pais> paises= new ArrayList<>();
+    Jugador jugadorUno;
+    Jugador jugadorDos;
+    Continente continente;
+
+    @BeforeEach
+    void setUp() {
+        paises.add(new Pais("Borneo", List.of("Australia","Malasia")));
+        paises.add(new Pais("Sumatra", List.of("Australia","India")));
+        paises.add(new Pais("Australia", List.of("Chile","Sumatra","Java","Borneo")));
+        paises.add(new Pais("Java", List.of("Australia")));
+        continente = new Continente("Oceania",2,paises);
+        jugadorUno = new Jugador("Julian");
+        jugadorDos = new Jugador("Sofia");
+    }
+
+    @Test
+    public void contineteSeCreaConElNombreCorrecto(){
+        assertEquals("Oceania",continente.getNombre());
+    }
+
+    @Test
+    public void contineteSeCreaConLaCantidadDeFichasExtraCorrecta(){
+
+        assertEquals(2,continente.getFichas());
+    }
+
+    @Test
+    public void contineteSeCreaConLosPaisesCorrectos(){
+
+        assertEquals(paises,continente.getPaises());
+    }
+}
