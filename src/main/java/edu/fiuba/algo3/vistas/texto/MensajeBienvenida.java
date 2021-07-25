38ede5a9912ff90a0c1ba46209eeb66d64014972
Andreskub <andreskubler@gmail.com>
2021-07-25 18:11:18 -0300

    inicializo un mensaje en el menu de bienvenida

---
+++ src/main/java/edu/fiuba/algo3/vistas/texto/MensajeBienvenida.java
@@ -0,0 +1,16 @@
+package edu.fiuba.algo3.vistas.texto;
+
+import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+
+public class MensajeBienvenida extends Label {
+
+    public MensajeBienvenida(){//(String color){
+        super("ALTEGO");
+        //super.setFont(Font.font(EstilosApp.FUENTE,80));
+        //super.setTextFill(Color.web(color));
+        //super.setWrapText(true);
+    }
+
+}
