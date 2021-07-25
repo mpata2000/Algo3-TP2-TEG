db32c973c89f36db3dbe7d86ac116fa3ea316487
Andreskub <andreskubler@gmail.com>
2021-07-25 18:10:59 -0300

    Modifico main para poder correr desde ahi

--- src/main/java/edu/fiuba/algo3/Main.java
+++ src/main/java/edu/fiuba/algo3/Main.java
@@ -1,13 +1,29 @@
 package edu.fiuba.algo3;
 
-import edu.fiuba.algo3.lector.LectorDeJson;
+import edu.fiuba.algo3.vistas.ContenedorPrincipal;
+import edu.fiuba.algo3.vistas.VistaInicio;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application {
     public static void main(String[] args){
-        App.main(args);
-        LectorDeJson lector = new LectorDeJson();
+        launch(args);
+        //App.main(args); //Esto ya estaba aca
+    }
 
-        lector.lectorTablero("resources/Teg-Tablero.json");
-        lector.lectorCartasPais("resources/Teg-Cartas.json");
+    @Override
+    public void start(Stage stage) throws Exception{
+        stage.setTitle("ALTEGO");
+
+        ContenedorPrincipal contenedorPrincipal = new ContenedorPrincipal(stage);
+        contenedorPrincipal.setCentro(new VistaInicio(stage,contenedorPrincipal));
+
+        //var label = new Label("Bienvenido al juego ALTEGO");
+        var scene = new Scene(contenedorPrincipal, 640, 480);
+        stage.setScene(scene);
+
+        stage.show();
     }
 }
