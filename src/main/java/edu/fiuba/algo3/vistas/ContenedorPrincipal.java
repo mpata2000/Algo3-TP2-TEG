08013d5040f3c4d7d441d1c2b10bbbfdcf59d1b1
Andreskub <andreskubler@gmail.com>
2021-07-25 23:15:49 -0300

    Refactor del contenedor principal

--- src/main/java/edu/fiuba/algo3/vistas/ContenedorPrincipal.java
+++ src/main/java/edu/fiuba/algo3/vistas/ContenedorPrincipal.java
@@ -8,13 +8,13 @@ import javafx.stage.Stage;
 
 public class ContenedorPrincipal extends BorderPane {
 
+    /*
     public ContenedorPrincipal(Stage stage) {
-
         Button botonEnviar = new Button();
         botonEnviar.setText("BotonDePrueba");
-        botonEnviar.setAlignment(Pos.CENTER);
+        botonEnviar.setAlignment(Pos.TOP_RIGHT);
         this.setTop(botonEnviar);
-    }
+    }*/
 
     public void setCentro(StackPane centro){
         this.setCenter(centro);
