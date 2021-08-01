7cb50aa4e508eed7a55d2331b89ae9218603b4f8
Andreskub <andreskubler@gmail.com>
2021-08-01 16:51:03 -0300

    Refactor

--- src/main/java/edu/fiuba/algo3/vistas/grillas/MenuInicioGrid.java
+++ src/main/java/edu/fiuba/algo3/vistas/grillas/MenuInicioGrid.java
@@ -2,24 +2,15 @@ package edu.fiuba.algo3.vistas.grillas;
 
 import javafx.geometry.Pos;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.RowConstraints;
 
 public class MenuInicioGrid extends GridPane {
-
-    private static int DISTANCIA_FILAS = 15;
-    private static int TAM_ENCABEZADO = 110;
-    private static int TAM_SECCION_PREGUNTA = 375;
-    private static int TAM_BOTON_ENVIAR = 50;
+    private static int GAP_FILAS = 15;
 
     public MenuInicioGrid(double screenWidth, double screenHeight) {
-
+        //super.setGridLinesVisible(true);
         super.setMinSize(screenWidth + 50, screenHeight);
         super.setMaxSize(screenWidth + 50, screenHeight);
         super.setAlignment(Pos.CENTER);
-        super.setVgap(DISTANCIA_FILAS);
-        super.getRowConstraints().add(new RowConstraints(TAM_ENCABEZADO));
-        super.getRowConstraints().add(new RowConstraints(TAM_SECCION_PREGUNTA));
-        super.getRowConstraints().add(new RowConstraints(TAM_BOTON_ENVIAR));
-
+        super.setVgap(GAP_FILAS);
     }
 }
