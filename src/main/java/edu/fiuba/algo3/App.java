e6e1e1a67b572b61374da5f4a3cd7ea3f2aba142
sofi <sofiamarchesini@hotmail.com>
2021-08-03 15:23:50 -0300

    Comienzo de uso de scene builder

--- src/main/java/edu/fiuba/algo3/App.java
+++ src/main/java/edu/fiuba/algo3/App.java
@@ -3,8 +3,12 @@ package edu.fiuba.algo3;
 import edu.fiuba.algo3.vistas.ContenedorPrincipal;
 import edu.fiuba.algo3.vistas.MenuInicioView;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 /**
  * JavaFX App
@@ -12,8 +16,14 @@ import javafx.stage.Stage;
 public class App extends Application {
 
     @Override
-    public void start(Stage stage){
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/vistas/tablero.fxml"));
         stage.setTitle("ALTEGO");
+        stage.setScene(new Scene(root, 1080, 800));
+        stage.show();
+
+
+        /*stage.setTitle("ALTEGO");
 
         ContenedorPrincipal contenedorPrincipal = new ContenedorPrincipal(stage);
         contenedorPrincipal.setCentro(new MenuInicioView(stage,contenedorPrincipal));
@@ -21,12 +31,12 @@ public class App extends Application {
         var scene = new Scene(contenedorPrincipal, 1080, 800);
         stage.setScene(scene);
 
-        stage.show();
+        stage.show();*/
 
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 
 }
