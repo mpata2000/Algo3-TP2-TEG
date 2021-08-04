6312abde5b8aee6290da9561701b321f5cdbffc3
Santiago Vaccarelli <svaccarelli@fi.uba.ar>
2021-08-03 21:03:22 -0300

    Agrego controles para los botones colocar atacar y pasar

--- src/main/java/edu/fiuba/algo3/controller/ControllerTablero.java
+++ src/main/java/edu/fiuba/algo3/controller/ControllerTablero.java
@@ -1,4 +1,38 @@
 package edu.fiuba.algo3.controller;
 
+import edu.fiuba.algo3.modelo.Turnos;
+import edu.fiuba.algo3.vistas.CargadorDeEscena;
+import javafx.stage.Stage;
+
 public class ControllerTablero {
+    public String fichas;
+    public String paisOrigen;
+    public String paisDestino;
+
+
+    public void colocar(){
+        Turnos.getInstance().colocarFichas(paisDestino,Integer.parseInt(fichas));
+        CargadorDeEscena.cargarEscena("/vistas/añadirJugadores.fxml");
+    }
+
+
+    public void atacar(){
+        Turnos.getInstance().atacarACon(paisOrigen,paisDestino,Integer.parseInt(fichas));
+        CargadorDeEscena.cargarEscena("/vistas/añadirJugadores.fxml");
+    }
+
+
+    public void pasar(){
+        Turnos.getInstance().pasarFichas(paisOrigen,paisDestino,Integer.parseInt(fichas));
+        CargadorDeEscena.cargarEscena("/vistas/añadirJugadores.fxml");
+    }
+
+    public void pasarRonda(){
+        Turnos.getInstance().finEtapa(); /* Pasar turno */
+        CargadorDeEscena.cargarEscena("/vistas/añadirJugadores.fxml");
+    }
+
+    public void mostrarObjetivo(){
+        Turnos.getInstance().obejtivoMostrar();
+    }
 }
